6d22d42c888931decdab469ff3f0af4adb44fa17
yunzhuchen0123 <yunzhuchen0123@gmail.com>
2026-07-20 22:19:02 -0700

    fix password hashing hook for mongoose 8

--- backend/models/User.js
+++ backend/models/User.js
@@ -25,10 +25,9 @@ const userSchema = new mongoose.Schema({
   }]
 }, { timestamps: true });
 
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
 userSchema.methods.comparePassword = async function(password) {
